5f48da1c7112f58d25ea944ced439e227cfa9401
Prutserdt <walter@elffrink.nl>
2026-02-10 20:31:40 +0100

    Added doom packages.el

---
+++ .config/doom/packages.el
@@ -0,0 +1,20 @@
+;; -*- no-byte-compile: t; -*-
+
+;; NOTE: This file is generated from ~/.config/doom/README.org
+;;      Please only edit that file and org-babel-tangle (emacs)
+
+(package! visual-fill-column)
+
+(package! org-babel-eval-in-repl)
+(package! eval-in-repl)
+(package! jupyter)
+
+(package! org-auto-tangle)
+
+(package! vterm-toggle)
+
+(package! gptel)
+(package! python-mode)
+
+(unpin! org-roam) ; unpinning is necessary for Roam emacs according to de developer of org-roam-ui
+(package! org-roam-ui)
