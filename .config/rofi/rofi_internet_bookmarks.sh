3cc0dab464e0de5f60265fbe2d0bed5fb2664533
Prutserdt <walter@elffrink.nl>
2026-03-06 23:19:29 +0100

    Updated

--- .config/rofi/rofi_internet_bookmarks.sh
+++ .config/rofi/rofi_internet_bookmarks.sh
@@ -9,20 +9,21 @@ bookmarks=("$HOME/stack/Command_line/urls")
 # Create a formatted string for rofi input preserving order and empty lines
 url=$(cat "${bookmarks[0]}" | rofi -i -dmenu -show -width 1500 -l 20 -p 'Bookmarks')
 
-# Get the workspace number of the Brave browser
-output=$(hyprctl clients)
-brave_workspace=$(echo "$output" | grep -A 7 -i "Brave" | grep "workspace:" | awk '{print $2}')
+# Only proceed if a URL was selected
+if [[ -n "$url" ]]; then
+    # Get the workspace number of the Brave browser
+    output=$(hyprctl clients)
+    brave_workspace=$(echo "$output" | grep -A 7 -i "Brave" | grep "workspace:" | awk '{print $2}')
 
-# Get the window ID of the Emacs client
-brave_window_id=$(echo "$output" | grep -i "Brave" | awk '{print $2}')
+    # Get the window ID of the Brave browser
+    brave_window_id=$(echo "$output" | grep -i "Brave" | awk '{print $2}')
 
-# Check if the Emacs window ID is non-empty
-if [[ -z "$brave_window_id" ]]; then
-    #echo "No Emacs workspace found. Starting emacsclient..."
-    brave --password-store=basic "$url"
-else
-    #echo "Switching to Emacs workspace $emacs_workspace and focusing on window $emacs_window_id..."
-    hyprctl dispatch workspace "$brave_workspace"
-    hyprctl dispatch focuswindow class:brave-browser
-    brave --password-store=basic "$url"
+    # Check if the Brave window ID is non-empty
+    if [[ -z "$brave_window_id" ]]; then
+        brave --password-store=basic "$url"
+    else
+        hyprctl dispatch workspace "$brave_workspace"
+        hyprctl dispatch focuswindow class:brave-browser
+        brave --password-store=basic "$url"
+    fi
 fi
